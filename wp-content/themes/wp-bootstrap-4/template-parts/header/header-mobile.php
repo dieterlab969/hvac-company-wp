fd97c69fa3b295c902f03dba0d206f42a26a437a
Dieter Roark <epictetus@kenkavn.com>
2025-02-03 09:03:07 +0000

    node/965-Fix-the-menu-bar-on-mobile-phone

--- wp-content/themes/wp-bootstrap-4/template-parts/header/header-mobile.php
+++ wp-content/themes/wp-bootstrap-4/template-parts/header/header-mobile.php
@@ -1,16 +1,18 @@
 <div class="header__mobile d-lg-none d-xl-none">
 	<div class="container">
 		<div class="row align-items-center">
-			<div class="col-2">
+			<div class="col-3">
 				<?php wp_nav_menu( array( 'theme_location' => 'menu-1') ); ?>
 			</div>
-			<div class="col-8 text-center">
+			<div class="col-5 text-center">
 				<a href="<?php bloginfo( 'url' ); ?>" class="header__logo__mobile">
 				    <?php $logo = get_field( 'logo', 'options' ); ?>
 					<img src="<?php echo $logo['url']; ?>" alt="Logo Website">
 				</a>
 			</div>
-			<div class="col-2"></div>
+			<div class="col-4 text-right">
+                <img src="<?php bloginfo('url'); ?>/wp-content/themes/wp-bootstrap-4/assets/images/phangia-hotline.png" alt="Hotline Number" class="hotline-img">
+            </div>
 		</div>
 	</div>
 </div>
