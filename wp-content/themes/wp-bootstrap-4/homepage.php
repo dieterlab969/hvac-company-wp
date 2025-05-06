6d8be5a55cc99a2a3023c50c12b0357b04ee537e
Dieter R <epictetus@kenkavn.com>
2025-05-06 21:10:48 +0700

    Node/1386-dienlanhphangia-com-layout-broke-of-the-Air-Conditioning-Solutions-block Upload missing code

--- wp-content/themes/wp-bootstrap-4/homepage.php
+++ wp-content/themes/wp-bootstrap-4/homepage.php
@@ -6,11 +6,14 @@ get_header(); ?>
         
         <?php get_template_part( 'template-parts/front-page/slide-homepage' ); ?>
         <?php get_template_part( 'template-parts/front-page/start' ); ?>
+        <?php get_template_part( 'template-parts/front-page/san-pham-noi-bat' ); ?>
+        <?php get_template_part( 'template-parts/front-page/ads' ); ?>
         <?php get_template_part( 'template-parts/front-page/service' ); ?>
         <!-- // get_template_part( 'template-parts/front-page/thanhly' ); -->
         
         <?php get_template_part( 'template-parts/front-page/congtrinh' ); ?>
         <?php get_template_part( 'template-parts/front-page/review' ); ?>
+        <?php get_template_part('template-parts/front-page/certificates'); ?>
         <?php get_template_part( 'template-parts/front-page/news' ); ?>
 
 
---
+++ wp-content/themes/wp-bootstrap-4/template-parts/front-page/ads.php
@@ -0,0 +1,75 @@
+<?php
+
+$grp_condition_solution = get_field('ads_on_frontpage', 'option');
+$condition_title = $grp_condition_solution['title'] ?? 'GiбєЈi phГЎp Д‘iб»Ѓu hoГ ';
+$condition_desc = $grp_condition_solution['description'] ?? 'Daikin lГ  nhГ  sбєЈn xuбєҐt mГЎy Д‘iб»Ѓu hГІa khГґng khГ­ hГ ng Д‘бє§u thбєї giб»›i vГ  cГЎc sбєЈn phбє©m cб»§a chГєng tГґi Д‘Ж°б»Јc bГЎn tбєЎi hЖЎn 140 quб»‘c gia. Kб»ѓ tб»« Д‘бє§u nhб»Їng nДѓm 1930, chГєng tГґi Д‘ГЈ tiбєїn mб»™t bЖ°б»›c dГ i trong viб»‡c hiб»‡n thб»±c hГіa mб»™t mГґi trЖ°б»ќng vГ  chбєҐt lЖ°б»Јng cuб»™c sб»‘ng tб»‘t hЖЎn thГґng qua viб»‡c cung cбєҐp cГЎc giбєЈi phГЎp Д‘iб»Ѓu hГІa khГґng khГ­. KhГЎm phГЎ tiбєїng nГіi cб»§a khГЎch hГ ng, cГґng nghб»‡ quan trб»Ќng vГ  cГўu chuyб»‡n cб»§a chГєng tГґi. KhГЎm phГЎ Daikin.';
+
+$banner_1 = $grp_condition_solution['banner_1'];
+$banner_2 = $grp_condition_solution['banner_2'];
+$banner_3 = $grp_condition_solution['banner_3'];
+$banner_4 = $grp_condition_solution['banner_4'];
+$banner_5 = $grp_condition_solution['banner_5'];
+$banner_6 = $grp_condition_solution['banner_6'];
+
+$b_title_1 = $banner_1['title'] ?: 'Cб»­a hГ ng tiб»‡n lб»Јi';
+$b_title_2 = $banner_2['title'] ?: 'KhГЎch sбєЎn nghб»‰ dЖ°б»Ўng';
+$b_title_3 = $banner_3['title'] ?: 'VДѓn phГІng';
+$b_title_4 = $banner_4['title'] ?: 'NhГ  phб»‘, villa ,biб»‡t thб»±';
+$b_title_5 = $banner_5['title'] ?: 'NhГ  hГ ng - KhГЎch sбєЎn';
+$b_title_6 = $banner_6['title'] ?: 'NhГ  mГЎy sбєЈn xuбєҐt';
+
+$b_img_1 = $banner_1['image'] ?: 'https://ad-daikin.daikin.com.vn:60444/storage/upload/media/ac-solutions/retail.png';
+$b_img_2 = $banner_2['image'] ?: 'https://ad-daikin.daikin.com.vn:60444/storage/upload/media/ac-solutions/resort-and-hotel-1.png';
+$b_img_3 = $banner_3['image'] ?: 'https://ad-daikin.daikin.com.vn:60444/storage/upload/media/ac-solutions/building.png';
+$b_img_4 = $banner_4['image'] ?: 'https://ad-daikin.daikin.com.vn:60444/storage/upload/media/ac-solutions/building.png';
+$b_img_5 = $banner_5['image'] ?: 'https://ad-daikin.daikin.com.vn:60444/storage/upload/media/ac-solutions/building.png';
+$b_img_6 = $banner_6['image'] ?: 'https://ad-daikin.daikin.com.vn:60444/storage/upload/media/ac-solutions/building.png';
+
+$b_link_1 = $banner_1['link'] ?: '#';
+$b_link_2 = $banner_2['link'] ?: '#';
+$b_link_3 = $banner_3['link'] ?: '#';
+$b_link_4 = $banner_4['link'] ?: '#';
+$b_link_5 = $banner_5['link'] ?: '#';
+$b_link_6 = $banner_6['link'] ?: '#';
+?>
+
+<div class="container">
+    <div class="advertisement-block">
+        <div class="left-column">
+            <h2 class="ad-title text-uppercase"><?= $condition_title ?></h2>
+            <p class="ad-description"><?= $condition_desc ?></p>
+        </div>
+        <div class="right-column">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="swiper swiper7">
+                            <div class="swiper-wrapper">
+                                <a class="sub-block swiper-slide" style="background-image: url('<?= esc_html($b_img_1) ?>');" href="<?= $b_link_1 ?>">
+                                    <span class="sub-title"><?= esc_html($b_title_1)?></span>
+                                </a>
+                                <a class="sub-block swiper-slide" style="background-image: url('<?= esc_html($b_img_2) ?>');" href="<?= $b_link_2 ?>">
+                                    <span class="sub-title"><?= esc_html($b_title_2) ?></span>
+                                </a>
+                                <a class="sub-block swiper-slide" style="background-image: url('<?= esc_html($b_img_3) ?>');" href="<?= $b_link_3 ?>">
+                                    <span class="sub-title"><?= esc_html($b_title_3) ?></span>
+                                </a>
+                                <a class="sub-block swiper-slide" style="background-image: url('<?= esc_html($b_img_4) ?>');" href="<?= $b_link_4 ?>">
+                                    <span class="sub-title"><?= esc_html($b_title_4)?></span>
+                                </a>
+                                <a class="sub-block swiper-slide" style="background-image: url('<?= esc_html($b_img_5) ?>');" href="<?= $b_link_5 ?>">
+                                    <span class="sub-title"><?= esc_html($b_title_5) ?></span>
+                                </a>
+                                <a class="sub-block swiper-slide" style="background-image: url('<?= esc_html($b_img_6) ?>');" href="<?= $b_link_6 ?>">
+                                    <span class="sub-title"><?= esc_html($b_title_6) ?></span>
+                                </a>
+                            </div>
+                            <div class="swiper-button-prev swiper-button-prev_7 fa fa-angle-left"></div>
+                            <div class="swiper-button-next swiper-button-next_7 fa fa-angle-right"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
